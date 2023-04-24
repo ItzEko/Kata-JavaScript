ec0a026717e5ad133e6f716229d1da6e71c990cb
itzelgt <itzelgt02@outlook.com>
2023-04-24 16:43:03 -0600

    tarea de meses del año

---
+++ condicionales/tarea-switch-mes/switch-case.js
@@ -0,0 +1,42 @@
+// 4. Hacer un switch case donde al escribir un numero de mes, nos imprima el nombre.
+
+var numero=prompt('Ingresa el numero del mes y te dire el nombre del mes')
+
+switch (numero) {
+    case '1':
+        window.alert('El nombre del mes es : ENERO')
+        break;
+    case '2':
+        window.alert('El nombre del mes es : FEBRERO')
+        break;
+    case '3':
+        window.alert('El nombre del mes es : MARZO')
+        break;
+    case '4':
+        window.alert('El nombre del mes es : ABRIL')
+        break;
+    case '5':
+        window.alert('El nombre del mes es : MAYO')
+        break;
+    case '6':
+        window.alert('El nombre del mes es : JUNIO')
+        break;
+    case '7':
+        window.alert('El nombre del mes es : JULIO')
+        break;
+    case '8':
+        window.alert('El nombre del mes es : AGOSTO')
+        break;
+    case '9':
+        window.alert('El nombre del mes es : SEPTIEMBRE')
+        break;
+    case '10':
+        window.alert('El nombre del mes es : OCTUBRE')
+        break;
+    case '11':
+        window.alert('El nombre del mes es : NOVIEMBRE')
+        break;
+    case '12':
+        window.alert('El nombre del mes es : DICIEMBRE')
+        break;
+}
